bfbbef22e0928a6de5912622f481d2c714805ed0
Ruslan <r.u.s.l.a.n_y.u.s.u.p.o.v@mail.ru>
2018-02-28 02:02:18 +0500

    The 2nd and 3rd points of 5th homework.
    The 5th homework is done.

--- gallery/index.php
+++ gallery/index.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/func.php';
 
+$imageAdd = require __DIR__ . '/upload.php';
+
 $images = getFilesFromFolder(__DIR__ . '/images');
 
 ?><!doctype html>
@@ -12,7 +14,7 @@ $images = getFilesFromFolder(__DIR__ . '/images');
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Галерея</title>
-    <link rel="stylesheet" href="/css/image.css">
+    <link rel="stylesheet" href="/gallery/css/image.css">
 </head>
 <body>
 
@@ -35,6 +37,16 @@ if ( is_array($images) ) {
     }
 }
 ?>
+<div class="gallery_form">
+    <h2>Форма загрузки изображений</h2>
+    <form action="/gallery/" method="post" enctype="multipart/form-data">
+        <input type="file" name="img">
+        <button type="submit">Добавить изображение</button>
+    </form>
+    <?php
+    if  ($imageAdd !== null) {?><div><?php echo $imageAdd; ?></div><?php }?>
+
+</div>
 
 </body>
 </html>
